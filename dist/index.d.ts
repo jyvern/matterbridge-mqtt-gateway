758635269eeb75c1f116f78e5ce332f5507219d8
jean-yves <jean-yves@perso.ovh>
2026-02-27 17:00:33 +0100

    chore: add .gitignore and remove tracked build artifacts

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { MqttPlatform } from './platform.js';
-export default function (matterbridge: any, log: any, config: any): MqttPlatform;
-//# sourceMappingURL=index.d.ts.map
--- dist/index.js
+++
@@ -1,5 +0,0 @@
-import { MqttPlatform } from './platform.js';
-export default function (matterbridge, log, config) {
-    return new MqttPlatform(matterbridge, log, config);
-}
-//# sourceMappingURL=index.js.map
